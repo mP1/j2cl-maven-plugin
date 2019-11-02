fd02d97519a19594f6012f2901624b1c0bddc6f7
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2019-11-02 11:28:22 +1100

    Integration improvements N fixes

--- src/it/example-hello-world-reactor/example-hello-world-reactor-app/src/main/java/example/helloworld/app.js
+++ src/it/example-hello-world-reactor/example-hello-world-reactor-app/src/main/java/example/helloworld/app.js
@@ -18,6 +18,6 @@ goog.module('example.helloworld.app');
 // Feel free to use any of the fancy new JS features; Closure compiler will take
 // care of making your app run in an older browser if needed.
 //var {sayHello} = goog.require('j2cl.samples.hello');
-var {sayHello} = goog.require('example.helloworld.hello');
+var {sayHello} = goog.require('example.helloworldlib.hello');
 
 sayHello();
